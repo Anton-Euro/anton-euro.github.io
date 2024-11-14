c678cbadc45fa2321e57a8b0d1cffe4c8c8dce22
Geml <pirigovanton906@gmail.com>
2024-11-15 00:00:48 +0300

    t

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchData();
-    let tg = window.Telegram.WebApp;  
-    document.getElementById("butt1").onclick = function() {
-        console.log(tg.initDataUnsafe);
-        document.getElementById("text").innerText = tg.initDataUnsafe;
-        //tg.showAlert(`Добро пожаловать, @${tg.initData}.`);
-        //tg.openTelegramLink("https://t.me/BybitCoinsweeper_Bot?startapp");
-    }
+    
 });
+
+let tg = window.Telegram.WebApp;  
+document.getElementById("butt1").onclick = function() {
+    console.log(tg.initDataUnsafe);
+    document.getElementById("text").innerText = tg.initDataUnsafe;
+    //tg.showAlert(`Добро пожаловать, @${tg.initData}.`);
+    //tg.openTelegramLink("https://t.me/BybitCoinsweeper_Bot?startapp");
+}
 
 function fetchData() {
     const apiUrl = "https://f7f8-146-120-15-57.ngrok-free.app/webapps?user_id=1344042437";
@@ -34,10 +36,14 @@ function displayCards(data) {
         const card = document.createElement("div");
         card.className = "card";
         card.innerHTML = `
-            <a onclick="tg.openTelegramLink('${item.bot_url}?startapp');" class="card-link"><img src="${item.photo_url}" alt="${item.title}"></a>
+            <a onclick="go_to_link('${item.bot_url}?startapp');" class="card-link"><img src="${item.photo_url}" alt="${item.title}"></a>
             <div class="card-title">${item.title}</div>
         `;
 
         container.appendChild(card);
     });
+}
+
+function go_to_link(href) {
+    tg.openTelegramLink(href);
 }
