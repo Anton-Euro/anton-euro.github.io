f97ccecd0ed88a770a8701b5f1ca16ea4db32dd5
Geml <pirigovanton906@gmail.com>
2024-11-15 00:09:28 +0300

    t

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetchData();
-    
-});
+    let tg = window.Telegram.WebApp;
 
-let tg = window.Telegram.WebApp;  
-document.getElementById("butt1").onclick = function() {
-    console.log(tg.initDataUnsafe);
-    document.getElementById("text").innerText = tg.initDataUnsafe;
-    //tg.showAlert(`Добро пожаловать, @${tg.initData}.`);
-    //tg.openTelegramLink("https://t.me/BybitCoinsweeper_Bot?startapp");
-}
 
-function fetchData() {
-    const apiUrl = "https://f7f8-146-120-15-57.ngrok-free.app/webapps?user_id=1344042437";
+    const apiUrl = "https://f7f8-146-120-15-57.ngrok-free.app/webapps?user_id="+tg.initDataUnsafe.user.id;
 
     fetch(apiUrl, {
             method: 'GET',
@@ -26,24 +16,25 @@ function fetchData() {
             displayCards(data);
         })
         .catch(error => console.error("Ошибка при загрузке данных:", error));
-}
 
-function displayCards(data) {
-    const container = document.getElementById("menu");
-    container.innerHTML = "";
 
-    data.forEach(item => {
-        const card = document.createElement("div");
-        card.className = "card";
-        card.innerHTML = `
-            <a onclick="go_to_link('${item.bot_url}?startapp');" class="card-link"><img src="${item.photo_url}" alt="${item.title}"></a>
-            <div class="card-title">${item.title}</div>
-        `;
+    function displayCards(data) {
+        const container = document.getElementById("menu");
+        container.innerHTML = "";
 
-        container.appendChild(card);
-    });
-}
+        data.forEach(item => {
+            const card = document.createElement("div");
+            card.className = "card";
+            card.innerHTML = `
+                <a onclick="go_to_link('${item.bot_url}?startapp');" class="card-link"><img src="${item.photo_url}" alt="${item.title}"></a>
+                <div class="card-title">${item.title}</div>
+            `;
 
-function go_to_link(href) {
-    tg.openTelegramLink(href);
-}
+            container.appendChild(card);
+        });
+    }
+
+    function go_to_link(href) {
+        tg.openTelegramLink(href);
+    }
+});
