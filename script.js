190aa7850082dda18a011d98c5149f8e1458d241
Geml <pirigovanton906@gmail.com>
2024-11-14 23:40:40 +0300

    t

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+    let tg = window.Telegram.WebApp;
+        
+    document.getElementById("butt1").onclick = function() {
+        console.log(tg.initDataUnsafe);
+        document.getElementById("text").innerText = tg.initDataUnsafe;
+        //tg.showAlert(`Добро пожаловать, @${tg.initData}.`);
+        //tg.openTelegramLink("https://t.me/BybitCoinsweeper_Bot?startapp");
+    }
+});
+
+function fetchData() {
+    const apiUrl = "https://fc6c-146-120-15-57.ngrok-free.app/webapps?user_id=1344042437";
+
+    fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'token': 'qwe123'
+            }
+        }
+    )
+        .then(response => response.json())
+        .then(data => {
+            displayCards(data);
+        })
+        .catch(error => console.error("Ошибка при загрузке данных:", error));
+}
+
+function displayCards(data) {
+    const container = document.getElementById("menu");
+    container.innerHTML = "";
+
+    data.forEach(item => {
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+            <a href="${item.bot_url}?startapp" class="card-link" target="_blank"><img src="${item.photo_url}" alt="${item.title}"></a>
+            <div class="card-title">${item.title}</div>
+        `;
+
+        container.appendChild(card);
+    });
+}
