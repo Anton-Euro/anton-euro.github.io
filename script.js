1a633d88f38179e5bfddacc95b0c40fb0e27874e
Geml <pirigovanton906@gmail.com>
2024-11-15 20:12:03 +0300

    t

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 let tg = window.Telegram.WebApp;
+tg.expand();
 
-
-const apiUrl = "https://f7f8-146-120-15-57.ngrok-free.app/webapps?user_id=1344042437";
+const apiUrl = "https://eab2-146-120-15-57.ngrok-free.app/webapps?user_id="+tg.initDataUnsafe.user.id;
 
 fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-            'token': 'qwe123'
-        }
+    method: 'GET',
+    headers: {
+        'token': 'qwe123'
     }
-)
-    .then(response => response.json())
-    .then(data => {
-        displayCards(data);
-    })
-    .catch(error => console.error("Ошибка при загрузке данных:", error));
-
-
-function displayCards(data) {
+}).then(response => response.json()).then(data => {
     const container = document.getElementById("menu");
     container.innerHTML = "";
 
@@ -25,13 +16,14 @@ function displayCards(data) {
         const card = document.createElement("div");
         card.className = "card";
         card.innerHTML = `
-            <a onclick="go_to_link('${item.bot_url}?startapp');" class="card-link"><img src="${item.photo_url}" alt="${item.title}"></a>
+            <a onclick="go_to_link('${item.bot_url}?startapp');" class="card-link"><img src="data:image/png;base64,${item.photo_base64}" alt="${item.title}"></a>
             <div class="card-title">${item.title}</div>
         `;
 
         container.appendChild(card);
     });
-}
+}).catch(error => console.error("Ошибка при загрузке данных:", error));
+
 
 function go_to_link(href) {
     tg.openTelegramLink(href);
